7a0e2de6e8fe2ec0c87736ab3620161c7784e061
Nandani Raut <nandaniraut7@gmail.com>
2025-11-03 13:08:17 +0530

    Added Linked List programs

---
+++ LinkedListStack.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class LinkedListStack{
+    public class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static class Stack{
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int data){
+            Node newNode=new Node(data);
+            if(isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+        public static int pop(){
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+        public static int peek(){
+            return head.data;
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}
